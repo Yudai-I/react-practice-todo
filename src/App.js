416ce35101e332b2d851beb03e0c9727b010ca65
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2023-10-02 22:16:09 +0900

    [update]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
+import TodoList from "./TodoList";
+
 function App() {
+  const [todos, setTodos] = useState();
   return (
-    <div>
-<h1>hello</h1>
-    </div>
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
+    </>
   );
 }
 
