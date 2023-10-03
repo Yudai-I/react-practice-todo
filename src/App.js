5bb3cd4a26c496bf9b40ef6bdf947a96aca59ba1
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2023-10-03 23:38:47 +0900

    [Finish]

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [todos, setTodos] = useState([
-    { id: 1, name: "Todo1", completed: false }
+    
   ]);
   const todoNameRef = useRef();
   const handleAddTodo = () => {
     //タスクを追加する
     const name = todoNameRef.current.value;
+    if (name === "") return;
     setTodos((prevTodos) => {
       return [...prevTodos, {id: uuidv4(), name, completed: false }];
     });
@@ -23,13 +24,18 @@ function App() {
     setTodos(newTodos);
   };
 
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
+
 
   return (
     <>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input type="text" ref={todoNameRef} />
       <button onClick={handleAddTodo}>タスクを追加</button>
-      <button>完了したタスクの削除</button>
+      <button onClick={handleClear}>完了したタスクの削除</button>
       <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
     </>
   );
